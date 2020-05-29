eb35b776445f4fa699fc1d9f8866b60a01243d3d
TSecretT <timichfull@gmail.com>
2020-05-29 17:18:47 +0600

    Release v1.1.0

--- ESL.py
+++ ESL.py
@@ -9,47 +9,15 @@ import telebot
 from decimal import Decimal
 import os
 
-
 class ESL():
     folder_name = 'Items'
     PATH = f'{folder_name}/'
+    pool = ThreadPool(8)
+    base_url = 'https://shop.eslgaming.com'
 
-<<<<<<< HEAD
     if not os.path.exists(folder_name): os.path.mkdir(folder_name)
-=======
-def checkItem(link):
-    try:
-        j = json.loads(requests.get(link+'.js').text)
-    except ConnectionError:
-        print('Connection error')
-        return
-    except TimeoutError:
-        print('Connection error')
-        return
-    item={
-        'prase_time': time.strftime("%d:%m:%y-%H:%M:%S", time.gmtime(time.time())),
-        'id': j['id'],
-        'title': j['title'],
-        'handle': j['handle'],
-        'published': j['published_at'],
-        'type': j['type'],
-        'sale_price': float(j['price'] / 100),
-        'price': float(j['compare_at_price'] / 100),
-        'sizes': j['options'][0]['values'],
-        'url': j['url'],
-        'image_url': 'https://'+j['images'][0][2:],
-        'available': j['available']
-        }
-    return item
->>>>>>> 3203788ffc12cbc68a5b779a6800b496f1c7e17b
 
-    def __init__(self):
-        self.pool = ThreadPool(8)
-
-    def get_source(self, link):
-        return BeautifulSoup(self.sendRequest(link).content, 'lxml')
-    
-    def sendRequest(self, url):
+    def sendRequest(self, url): #
         while True:
             try:
                 r = requests.get(url)
@@ -58,93 +26,73 @@ def checkItem(link):
                 print(f'sendRequest () error {r.status_code} {e}')
                 time.sleep(5)
 
-<<<<<<< HEAD
-    def checkItem(self, link):
-        r = self.sendRequest(link+'.js')
-        item = r.json()
-        item['prase_time'] = time.strftime("%d:%m:%y-%H:%M:%S", time.gmtime(time.time())),
-        return item
+    def getSoup(self, link):
+        return BeautifulSoup(self.sendRequest(link).content, 'lxml')
 
-    def getItemsUrls(self, i):
-        item_urls = []
-        soup = self.get_source('https://shop.eslgaming.com/collections/all?page=' + str(i))
-        for item in soup.findAll('div', class_='grid-product__content'):
-            item_urls.append('https://shop.eslgaming.com'+item.a['href'])
-        return item_urls
-
-    def writeData(self, item):
-        try:
-            with open(self.PATH+item['handle']+'.json', 'r', encoding='utf-8') as file:
-                data=json.load(file)
-        except:
-            data=[]
-        finally:
-            with open(self.PATH+item['handle']+'.json', 'w', encoding='utf-8') as file:
-                data.append(item)
-                json.dump(data, file, indent=4)
-
-    def parse(self, anazyle=False):
-        soup = self.get_source('https://shop.eslgaming.com/collections/all')
+    def fetchUrls(self):
+        soup = self.getSoup(self.base_url + '/collections/all')
         total_pages = list(range(1,int(soup.find('span', class_='page').text.split('/')[1])+1))
-        results = self.pool.map(self.getItemsUrls, total_pages)
-        item_urls=[]
+        results = self.combineResults(self.pool.map(self.fetchSinglePage, total_pages))
+        return results
+
+    def combineResults(self, results): #
+        results_= []
         for result in results:
-            for item_url in result:
-                item_urls.append(item_url)
-        item_urls=item_urls[5:]
-        results = self.pool.map(self.checkItem, item_urls)
-        for result in results:
-            if anazyle:
-                self.anazyle_data(result)
-            self.writeData(result)
-=======
-def analyzeItems(item):
-    priceChange = False
-    sizeChange = False
-    stateChange = False
-    newItem = False
-    msg=''
-    try:
-        with open(PATH+item['handle']+'.json', 'r', encoding='utf-8') as file:
-            data=json.load(file)[-1]
-        if item['sale_price'] != data['sale_price']:    priceChange = True
-        if item['sizes'] != data['sizes']:  sizeChange = True
-        if item['available'] != data['available']:  stateChange = True
-    except FileNotFoundError:
-        newItem = True
-        return
-    finally:
-        if stateChange: msg=msg+'Available:  '+str(item['available'])
-        if priceChange: msg=msg+'Price changed from {}€ to {}€\n'.format(item['sale_price'], data['sale_price'])
-        if sizeChange: msg=msg+'Sizes changed {}\n'.format(set(item['sizes']) - set(data['sizes']))
-        if stateChange or priceChange or sizeChange:
-            urllib.request.urlretrieve(item['image_url'], 'image.jpg')
-            img = open('image.jpg', 'rb')
-            bot.send_photo(admin_id, img, msg)
+            results_.extend(result)
+        return results_
 
+    def fetchSinglePage(self, page_num):  
+        urls = []
+        soup = self.getSoup(self.base_url + '/collections/all?page=' + str(page_num))
+        for item in soup.findAll('div', class_='grid-product__content'):
+            urls.append(self.base_url + item.a['href'])
+        return urls
 
+    def fetchItems(self, bot=False): # main function to get data of all items in the shop
+        msgs=[]
+        itemUrls = self.fetchUrls()
+        results =self.pool.map(self.fetchItem, itemUrls)
+        for item in results:
+            msg = self.compareItem(item)
+            msgs.append(msg)
+            self.saveData(item)
+        if bot:
+            return msgs
 
-def check_items(analyze=False):
-    print("Parsing ESL items...")
-    starttime = time.time()
-    soup = get_source('https://shop.eslgaming.com/collections/all')
-    total_pages = list(range(1,int(soup.find('span', class_='page').text.split('/')[1])+1))
-    pool = ThreadPool(8)
-    results = pool.map(getItemUrls, total_pages)
-    item_urls=[]
-    for result in results:
-        for item_url in result:
-            item_urls.append(item_url)
-    results = pool.map(checkItem, item_urls)
-    print('Saving data...')
-    if not os.path.exists('Items/'):    os.mkdir('Items')
-    for result in results:
-        if analyze: analyzeItems(result)
-        writeData(result)
-    try:
-        os.remove("image.jpg")
-    except:
-        pass
-    finishtime = time.time()
-    print('Done in ' + str(round(finishtime-starttime, 0)))
->>>>>>> 3203788ffc12cbc68a5b779a6800b496f1c7e17b
+    def fetchItem(self, url): #fetch data of an items
+        res = self.sendRequest(url+'.js').json()
+        res['parse_time'] = time.strftime("%d:%m:%y-%H:%M:%S", time.gmtime(time.time()))
+        res['parse_time_unix'] = time.time()
+        return res
+
+    def saveData(self, item): #save list of items into json
+        if os.path.exists(self.PATH+str(item['id'])):
+            with open(f"{self.PATH}{item['id']}.json") as file:
+                data = json.load(file)
+        else:
+            data = []
+        data.append(item)
+        with open(f"{self.PATH}{item['id']}.json", 'w') as file:
+            _ = json.dump(data, file, indent=4)
+
+    def compareItem(self, item):
+        priceChange = False
+        sizeChange = False
+        stateChange = False
+        newItem = False
+        msg = ''
+        try:
+            with open(f"{self.PATH}{item['id']}.json") as file:
+                item_old = json.load(file)[-1]
+            if item['price'] != item_old['price']:    priceChange = True
+            #if item['sizes'] != item_old['sizes']:  sizeChange = True
+            if item['available'] != item_old['available']:  stateChange = True
+        except FileNotFoundError:
+            newItem = True
+        finally:
+            if stateChange: msg=msg+'Available:  '+str(item['available'])
+            if priceChange: msg=msg+'Price changed from {}€ to {}€\n'.format(item_old['price']/100, item['price']/100)
+            #if sizeChange: msg=msg+'Sizes changed {}\n'.format(set(item['sizes']) - set(item_old['sizes']))
+            if newItem: msg=msg+f"New item {item['price']/100}"
+            item['msg'] = msg
+            return item
